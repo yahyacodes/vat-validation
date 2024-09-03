5c5fe6f6e3ec8ad86a3eb1bc4da9f5e9736182cf
yahyacodes <yahya.codin@gmail.com>
2024-09-03 12:58:05 +0300

    Validate VAT Number

--- app/page.js
+++ app/page.js
@@ -1,114 +1,88 @@
-import Image from "next/image";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
-  // function to fetch data from https://api.vatcomply.com/vat?vat_number=NL810462783B01
+  const [vatData, setVatData] = useState(null);
+  const [vatNumber, setVatNumber] = useState("");
+  const [error, setError] = useState(null);
+
+  const fetchVatData = async (e) => {
+    e.preventDefault();
+
+    if (!vatNumber) {
+      setError("Please enter a VAT number.");
+      return;
+    }
+
+    setError(null);
+
+    // API URL for fetching VAT details NL810462783B01
+    const apiUrl = `https://api.vatcomply.com/vat?vat_number=${vatNumber}`;
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(apiUrl);
+        setVatData(response.data);
+      } catch (err) {
+        setError(err);
+      }
+    };
+    fetchData();
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-4xl">VAT Number Validation</h1>
+      <p className="max-w-lg text-center leading-snug">
+        You can verify the validity of a VAT number by entering the number to be
+        validated.
+      </p>
+      {error && <p className="text-red-500 mb-4">{error}</p>}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <form onSubmit={fetchVatData} className="flex gap-4 mt-10">
+        <input
+          type="text"
+          value={vatNumber}
+          onChange={(e) => setVatNumber(e.target.value)}
+          placeholder="Enter VAT number"
+          className="input border p-2 border-zinc-950 w-[20rem] max-w-xs"
         />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* Button to fetch VAT data */}
+        <button
+          className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-zinc-950 text-white px-6 p-1"
+          type="submit"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          Validate
+        </button>
+      </form>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+      {vatData && (
+        <div className="border-2 border-zinc-950 p-10 mt-10 max-w-3xl grid grid-cols-2">
+          <p className="p-2">
+            <strong>Valid:</strong>
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+          <p className="p-2">{vatData.valid ? "Yes" : "No"}</p>
+          <p className="p-2">
+            <strong>VAT Number:</strong>
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <p className="p-2">{vatData.vat_number}</p>
+          <p className="p-2">
+            <strong>Company Name:</strong>
           </p>
-        </a>
-      </div>
+          <p className="p-2">{vatData.name || "N/A"}</p>
+          <p className="p-2">
+            <strong>Address:</strong>
+          </p>
+          <p className="p-2">{vatData.address || "N/A"}</p>
+          <p className="p-2">
+            <strong>Country Code:</strong>
+          </p>
+          <p>{vatData.country_code}</p>
+        </div>
+      )}
     </main>
   );
 }
